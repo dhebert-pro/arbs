193a57836ee701eb27845db91b4bf8254267a46c
Sourivore <sourivore@gmail.com>
2017-12-11 20:42:04 +0100

    Fix line endings

--- src/components/stories/Footer.stories.js
+++ src/components/stories/Footer.stories.js
@@ -2,6 +2,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Footer from "components/Footer";
 
-const stories = storiesOf("Footer", module);;
+const stories = storiesOf("Footer", module);
 
 stories.add("default", () => <Footer />);
--- src/components/stories/Header.stories.js
+++ src/components/stories/Header.stories.js
@@ -2,6 +2,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Header from "components/Header";
 
-const stories = storiesOf("Header", module);;
+const stories = storiesOf("Header", module);
 
 stories.add("default", () => <Header />);
